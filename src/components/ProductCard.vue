9e6f010615daa40896af7c24074fcb529ed27b03
Gustavo Castillo Nájera <gcnctz@gmail.com>
2024-04-25 15:58:08 -0600

    ADD CARROUSEL PRODUCT CARD

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="product-card__container">
+    <img
+      class="product-card__image"
+      src="../assets/images/Imagen de producto 04 652 x 630.jpg"
+      alt=""
+    />
+    <section class="product-card__description">
+      <p class="product-card__price">999.00 MXN</p>
+      <p class="product-card__title">
+        Solitario De Oro Amarillo Estilo mexicanos nacionaleis
+      </p>
+      <p class="product-card__subtitle">14K Con 20Pts De Diamante (Si) (H)</p>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.product-card__container {
+  height: 474px;
+  width: 288px;
+  border-radius: 8px;
+  border: 1px solid #e3e4eb;
+}
+
+.product-card__description {
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 15px;
+}
+.product-card__image {
+  width: 100%;
+  height: 360px;
+  object-fit: cover;
+  border-radius: 8px 8px 0 0;
+}
+
+.product-card__price {
+  padding: 0;
+  margin: 0;
+  margin-bottom: 5px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
+  color: #2d2e36;
+}
+
+.product-card__price::before {
+  content: "$";
+  margin-right: 7px;
+}
+
+.product-card__title {
+  padding: 0;
+  margin: 0;
+  margin-bottom: 5px;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18.2px;
+  letter-spacing: calc(14px * 0.03);
+  color: #4f4f4f;
+}
+
+.product-card__subtitle {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15.6px;
+  letter-spacing: calc(12px * 0.03);
+  color: #707279;
+}
+</style>
---
+++ src/components/Promotion.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="promotion__container">
+    <img class="promotion__img" src="../assets/images/promocion.jpg" alt="" />
+    <h2 class="promotion__title">
+      Encuentra el anillo ideal para tu persona especial
+    </h2>
+    <h3 class="promotion__subtitle">
+      Descubre nuestro increíble catálogo de anillos perfectos para cualquier
+      ocasión.
+    </h3>
+    <div class="promotion__button">
+      <span>Ir a sección</span>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.promotion__container {
+  margin-top: 100px;
+  position: relative;
+}
+.promotion__img {
+  width: 100%;
+  object-fit: cover;
+}
+
+.promotion__title,
+.promotion__subtitle,
+.promotion__button {
+  position: absolute;
+  margin-left: 60px;
+}
+.promotion__title {
+  top: 15%;
+  width: 50%;
+  font-family: "Playfair Display", serif;
+  font-weight: 600;
+  font-size: 44px;
+  line-height: 57.2px;
+  letter-spacing: calc(44px * 0.03);
+  color: #f4f4f4;
+}
+
+.promotion__subtitle {
+  top: 47%;
+  width: 35%;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23.4px;
+  letter-spacing: calc(18px * 0.03);
+  color: #cfcfcf;
+}
+
+.promotion__button {
+  top: 65%;
+  width: 376px;
+  height: 56px;
+  padding: 14px 32px 14px 32px;
+  background-color: #e3e4eb;
+  border-radius: 360px;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+}
+
+.promotion__button:hover {
+  cursor: pointer;
+}
+
+.promotion__button span {
+  color: #2f3033;
+  font-weight: 500;
+  font-size: 21px;
+  line-height: 27.7px;
+  letter-spacing: calc(21px * 0.035);
+}
+</style>
